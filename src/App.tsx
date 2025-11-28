07f6258b88d3bbad942b3f4d4b4b5224922c0094
Flogert <flogert@rocketmail.com>
2025-11-28 11:10:13 -0500

    Updates

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { Preloader } from "@/components/Preloader";
 import { lazy, Suspense } from "react";
 
@@ -17,7 +17,7 @@ const App = () => (
       <Preloader />
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <HashRouter>
         <Suspense fallback={null}>
           <Routes>
             <Route path="/" element={<Index />} />
@@ -25,7 +25,7 @@ const App = () => (
             <Route path="*" element={<NotFound />} />
           </Routes>
         </Suspense>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
